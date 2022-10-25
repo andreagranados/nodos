c486dab28ba167ad64843e9b3b95549f401fabbf
Andrea <Andrea@LAPTOP-CK2T37FA>
2022-10-25 09:39:14 -0300

    Login Inicial Cantidad de registros en operacion de Comparacion

---
+++ metadatos/tablas/apex_pagina_tipo.sql
@@ -0,0 +1,16 @@
+
+------------------------------------------------------------
+-- apex_pagina_tipo
+------------------------------------------------------------
+INSERT INTO apex_pagina_tipo (proyecto, pagina_tipo, descripcion, clase_nombre, clase_archivo, include_arriba, include_abajo, exclusivo_toba, contexto, punto_montaje) VALUES (
+	'nodos', --proyecto
+	'nodos', --pagina_tipo
+	'Pagina de Login Nodos', --descripcion
+	'tp_nodos', --clase_nombre
+	'tp_nodos.php', --clase_archivo
+	NULL, --include_arriba
+	NULL, --include_abajo
+	NULL, --exclusivo_toba
+	NULL, --contexto
+	'24'  --punto_montaje
+);
